417dc3b3742bd510af756c6bf32454c6d077b571
hc <hc@hc-PC>
2018-02-10 13:11:48 +0800

    测试

--- src/main/java/com/ssm/model/OperLog.java
+++ src/main/java/com/ssm/model/OperLog.java
@@ -17,7 +17,7 @@ public class OperLog implements Serializable {
 	private Long id;
 	private String userId;// '操作用户ID',
 	private String userName;// '操作人名称',
-	@JSONField (format="yyyy-MM-dd HH:mm:ss") 
+	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
 	private Date operTime;// '操作时间(yyyy-MM-dd HH:mm:ss)',
 	private String clientIp;// '登录IP',
 	private String reqUrl;// 访问url
@@ -25,4 +25,88 @@ public class OperLog implements Serializable {
 	private String operEvent;// 操作事件（删除，新增，修改，查询，登录，退出）',
 	private int operStatus;// '操作状态（1：成功，2：失败）',
 	private String logDesc;// 描述信息',
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Date getOperTime() {
+		return operTime;
+	}
+
+	public void setOperTime(Date operTime) {
+		this.operTime = operTime;
+	}
+
+	public String getClientIp() {
+		return clientIp;
+	}
+
+	public void setClientIp(String clientIp) {
+		this.clientIp = clientIp;
+	}
+
+	public String getReqUrl() {
+		return reqUrl;
+	}
+
+	public void setReqUrl(String reqUrl) {
+		this.reqUrl = reqUrl;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public String getOperEvent() {
+		return operEvent;
+	}
+
+	public void setOperEvent(String operEvent) {
+		this.operEvent = operEvent;
+	}
+
+	public int getOperStatus() {
+		return operStatus;
+	}
+
+	public void setOperStatus(int operStatus) {
+		this.operStatus = operStatus;
+	}
+
+	public String getLogDesc() {
+		return logDesc;
+	}
+
+	public void setLogDesc(String logDesc) {
+		this.logDesc = logDesc;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
--- src/main/java/com/ssm/utils/Result.java
+++ src/main/java/com/ssm/utils/Result.java
@@ -4,19 +4,44 @@ import lombok.Data;
 
 /**
  * 统一返回类接口
+ * 
  * @Autor jiangcaijun
  * @Date 2017/11/9
  * @Time 10:21
  */
 @Data
 public class Result {
-    /**
-     * 表示返回成功或失败，true为成功
-     */
-    private Boolean success = true;
+	/**
+	 * 表示返回成功或失败，true为成功
+	 */
+	private Boolean success = true;
 
-    private String msg;
+	private String msg;
 
-    private Object obj;
+	private Object obj;
+
+	public Boolean getSuccess() {
+		return success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
 
 }
